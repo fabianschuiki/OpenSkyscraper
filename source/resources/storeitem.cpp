c28c27b908313c71832d9b445c0fc11ad53578a9
Fabian Schuiki <fabianschuiki@axamblis.com>
2010-07-31 01:50:35 +0200

    disabled automatic unloading in StoreItem class due to some occasions where a texture would be released by an object and later re-requested by another, yet the texture was being unloaded since the first object's release caused the retainCount to drop to 1 which kicked off the unloading. This needs to be changed to something more intelligent... maybe texture garbage collection :D ?

--- source/resources/storeitem.cpp
+++ source/resources/storeitem.cpp
@@ -6,6 +6,7 @@ using namespace OSS;
 
 void StoreItem::release() {
 	Object::release();
-	if (retainCount == 1)
-		getStore()->queueItemForUnloading(this);
+	//disabled the following unloading since it led tu some strange behaviour and bugs
+	/*if (retainCount == 1)
+		getStore()->queueItemForUnloading(this);*/
 }
