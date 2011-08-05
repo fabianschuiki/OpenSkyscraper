0eeed687f8b1f8e4dd44a2c7407ab0aa711754f2
Fabian Schuiki <fabianschuiki@axamblis.com>
2011-08-06 00:49:41 +0200

    Basic render window is showing.

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,26 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::Window app(sf::VideoMode(800, 600, 32), "SFML Window");
-	sleep(3);
+	//Create the main window.
+	sf::RenderWindow app(sf::VideoMode(800, 600, 32), "OpenSkyscraper SFML");
+	
+	//Run the main loop.
+	while (app.IsOpened()) {
+		
+		//Process events.
+		sf::Event event;
+		while (app.GetEvent(event)) {
+			
+			//Close window.
+			if (event.Type == sf::Event::Closed) app.Close();
+		}
+		
+		//Clear the screen.
+		app.Clear();
+		
+		//Display whatever we have drawn so far
+		app.Display();
+	}
 	return 0;
 }
