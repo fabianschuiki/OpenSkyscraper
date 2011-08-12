61c32b7929256205550b9d7c3f98b21128c40196
Fabian Schuiki <fabianschuiki@axamblis.com>
2011-08-12 11:46:02 +0200

    SFML is actually pretty good at drawing strings. Maybe we should switch to SFML?

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,14 @@ int main()
 	//Create the main window.
 	sf::RenderWindow app(sf::VideoMode(800, 600, 32), "OpenSkyscraper SFML");
 	
+	//Create a string to be drawn.
+	sf::Font jura;
+	jura.LoadFromFile("../Resources/fonts/Jura-Regular.ttf", 30);
+	
+	sf::String text("Welcome to OpenSkyscraper!", jura);
+	text.SetColor(sf::Color(255, 255, 0));
+	text.Move(100, 200);
+	
 	//Run the main loop.
 	while (app.IsOpened()) {
 		
@@ -18,6 +26,9 @@ int main()
 		
 		//Clear the screen.
 		app.Clear();
+		
+		//Draw the text.
+		app.Draw(text);
 		
 		//Display whatever we have drawn so far
 		app.Display();
