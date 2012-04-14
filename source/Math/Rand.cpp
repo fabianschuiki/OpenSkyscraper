306fb51bc31be76694940add191779322f701ce1
Fabian Schuiki <fabian@schuiki.ch>
2012-04-14 10:46:20 +0200

    Added some documentation to randi and randd.

--- source/Math/Rand.cpp
+++ source/Math/Rand.cpp
@@ -4,21 +4,13 @@
 namespace OT {
 	namespace Math {
 		
+		/// Returns a random integer between min and max (inclusive).
 		int randi(int min, int max) 
 		{
 			return (rand() % (max - min + 1)) + min;
 		}
-
-		unsigned int randui(unsigned int min, unsigned int max)
-		{
-			return (rand() % (max - min + 1)) + min;
-		}
-
-		float randf(float min, float max)
-		{
-			return ((float)rand() / RAND_MAX) * (max - min) + min;
-		}
-
+		
+		/// Returns a random double between min and max (inclusive).
 		double randd(double min, double max)
 		{
 			return ((double)rand() / RAND_MAX) * (max - min) + min;
