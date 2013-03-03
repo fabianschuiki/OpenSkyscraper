4e983baa83b2ee5f839524e765fff6a2faa02a6b
hoshi10 <hoshi10@server.fake>
2013-03-03 19:59:12 +0800

    Updated CMake files to facilitate compilation on Windows.
    - Added code for linking to debug libraries for libRocket for Debug projects (otherwise there will be compile errors). Previously I had resorted to manually changing the libraries which were linked in the Debug projects. [FindRocket.cmake]
    - Added code for finding mspack library paths (pkg-config doesn't work very well on Windows). [CMakeLists.txt]
    - Removed FindCEGUI.cmake since it is no longer required.

--- thirdparty/FindRocket.cmake
+++ thirdparty/FindRocket.cmake
@@ -40,22 +40,54 @@ foreach (component ${Rocket_FIND_COMPONENTS})
 	set(component_cap ${component_f}${component_r})
 	
 	# try to find the library
+	# debug library
+    find_library(
+		ROCKET_${component_upper}_LIBRARY_DEBUG
+		NAMES Rocket${component_cap}_d
+		PATH_SUFFIXES lib64 lib
+		PATHS ${FIND_ROCKET_PATHS}
+	)
+				 
+	# release library
 	find_library(
-		ROCKET_${component_upper}_LIBRARY
+		ROCKET_${component_upper}_LIBRARY_RELEASE
 		NAMES Rocket${component_cap}
 		PATH_SUFFIXES lib64 lib
 		PATHS ${FIND_ROCKET_PATHS}
 	)
 	
 	# check whether we've found the library
-	if (ROCKET_${component_upper}_LIBRARY)
+	if (ROCKET_${component_upper}_LIBRARY_DEBUG OR ROCKET_${component_upper}_LIBRARY_RELEASE)
 		set(ROCKET_${component_upper}_FOUND TRUE)
+		
+		# if both are found, set ROCKET_XXX_LIBRARY to contain both
+        if (ROCKET_${component_upper}_LIBRARY_DEBUG AND ROCKET_${component_upper}_LIBRARY_RELEASE)
+            set(ROCKET_${component_upper}_LIBRARY debug     ${ROCKET_${component_upper}_LIBRARY_DEBUG}
+                                                  optimized ${ROCKET_${component_upper}_LIBRARY_RELEASE})
+        endif()
+
+        # if only one debug/release variant is found, set the other to be equal to the found one
+        if (ROCKET_${component_upper}_LIBRARY_DEBUG AND NOT ROCKET_${component_upper}_LIBRARY_RELEASE)
+            # debug and not release
+            set(ROCKET_${component_upper}_LIBRARY_RELEASE ${ROCKET_${component_upper}_LIBRARY_DEBUG})
+            set(ROCKET_${component_upper}_LIBRARY         ${ROCKET_${component_upper}_LIBRARY_DEBUG})
+        endif()
+        if (ROCKET_${component_upper}_LIBRARY_RELEASE AND NOT ROCKET_${component_upper}_LIBRARY_DEBUG)
+            # release and not debug
+            set(ROCKET_${component_upper}_LIBRARY_DEBUG ${ROCKET_${component_upper}_LIBRARY_RELEASE})
+            set(ROCKET_${component_upper}_LIBRARY       ${ROCKET_${component_upper}_LIBRARY_RELEASE})
+        endif()
 	else()
 		set(ROCKET_FOUND FALSE)
 		set(ROCKET_${component_upper}_FOUND FALSE)
 		set(ROCKET_${component_upper}_LIBRARY "")
 		set(FIND_ROCKET_MISSING "${FIND_ROCKET_MISSING} ROCKET_${component_upper}_LIBRARY")
 	endif()
+	
+	# mark as advanced
+    MARK_AS_ADVANCED(ROCKET_${component_upper}_LIBRARY
+                     ROCKET_${component_upper}_LIBRARY_RELEASE
+                     ROCKET_${component_upper}_LIBRARY_DEBUG)
 	
 	# add to the list of libraries
 	set(ROCKET_LIBRARIES ${ROCKET_LIBRARIES} "${ROCKET_${component_upper}_LIBRARY}")
