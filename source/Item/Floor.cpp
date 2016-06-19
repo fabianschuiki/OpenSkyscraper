4be74623571445e4561b42f6222ac70ce77a9f37
Matthew Harvey <matthewharveys@gmail.com>
2016-06-19 21:17:17 +0000

    Fix rendering of floors
    I suspect that there are still some corner cases that will be
    messed up regarding floors, but for right now they render correctly
    in all of the basic cases that I cared to try.
    The fix involves making sure that we were setting the sprites to
    the right coordinates.

--- source/Item/Floor.cpp
+++ source/Item/Floor.cpp
@@ -18,14 +18,15 @@ void Floor::init()
 	background.SetImage(floor);
 	background.setTextureRect(sf::IntRect(0, 0, 8, 36));
 	background.setOrigin(0, 36);
-	background.setScale(8 / floor.getSize().x, 36 / floor.getSize().y);
-	//background.Resize(0, 36);
+	background.setScale(8.0 / floor.getSize().x, 36.0 / floor.getSize().y);
+	background.setOrigin(0, floor.getSize().y);
+	background.setPosition(getPosition().x*8, -getPosition().y*36);
 
 	ceiling.SetImage(floor);
 	ceiling.setTextureRect(sf::IntRect(0, 0, 8, 12));
-	//ceiling.Resize(0, 12);
-	ceiling.setScale(8 / floor.getSize().x, 12 / floor.getSize().y);
-	ceiling.setOrigin(0, -getSize().y);
+	ceiling.setScale(8.0 / floor.getSize().x, 36.0 / floor.getSize().y);
+	ceiling.setOrigin(0, floor.getSize().y);
+	ceiling.setPosition(getPosition().x*8, -getPosition().y*36);
 
 	interval.insert(position.x);
 	interval.insert(getRect().maxX());
@@ -64,10 +65,8 @@ void Floor::render(sf::RenderTarget & target) const
 			right = *i;
 			if(left == right) continue;
 
-			//b.Resize((right - left) * 8.0f, 36.0f);
-			b.setScale((right - left) * 8.0f / b.getTextureRect().width, 36.0f / b.getTextureRect().height);
-			//b.SetX((left - position.x) * 8.0f);
-			b.setPosition((left - position.x) * 8.0f, b.getTextureRect().top);
+			b.setScale((right - left) * 8.0f / b.getTextureRect().width, 1);
+			b.setPosition((left * 8.0f), -getPosition().y * 36);
 			target.draw(b);
 		} else {
 			// Draw only ceiling sprite
@@ -77,10 +76,8 @@ void Floor::render(sf::RenderTarget & target) const
 			right = *i;
 			if(left == right) continue;
 
-			//c.Resize((right - left) * 8.0f, 12.0f);
-			c.setScale((right - left) * 8.0f / c.getTextureRect().width, 12.0f / c.getTextureRect().height);
-			//c.SetX((left - position.x) * 8.0f);
-			c.setPosition((left - position.x) * 8.0f, c.getTextureRect().top);
+			c.setScale((right - left) * 8.0f / c.getTextureRect().width, 1);
+			c.setPosition((left * 8.0f), -getPosition().y * 36);
 			target.draw(c);
 		}
 
